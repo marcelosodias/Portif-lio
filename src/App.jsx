c9ddd412fca87bc2ee29e46e84f941116228f7de
Marcelo <diasoares96@gmail.com>
2024-12-09 14:06:36 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Home from "./pages";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,45 @@
+function Home() {
+    const scrollToSection = (id) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+    return (
+        <>
+        {/* Menu de navegação */}
+        <nav>
+            <ul>
+                <li><button onClick={() => scrollToSection("header")}>Home</button></li>
+                <li><button onClick={() => scrollToSection("container")}>Projetos</button></li>
+                <li><button onClick={() => scrollToSection("about")}>Sobre mim</button></li>
+                <li><button onClick={() => scrollToSection("contact")}>Contato</button></li>
+            </ul>
+        </nav>
+
+        <header id ="header">
+            <div className="text">
+                <h1>Portifolio</h1>
+                <p>Seja bem vindo ao meu portifolio</p>
+            </div>
+        </header>
+
+        <section id="container">
+            <h2>Projetos</h2>
+            <p>Em breve</p>
+        </section>
+
+        <section id="about">
+            <h2>Sobre mim</h2>
+            <p>Em breve</p>
+        </section>
+
+        <section id="contact">
+            <h2>Contato</h2>
+            <p>Em breve</p>
+        </section>
+        </>
+    );
+}
+
+export default Home;
